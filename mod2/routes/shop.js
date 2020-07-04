20a5213239b3b95de89d5f996ff82164bf742241
rakeshgzp <rprakeshprasad00@gmail.com>
2020-07-05 00:09:05 +0530

    Dynamic routes added

--- mod2/routes/shop.js
+++ mod2/routes/shop.js
@@ -7,7 +7,9 @@ const shopController = require('../controllers/shop')
 router.get('/', shopController.getIndex);
 router.get('/products',shopController.getProducts);
 router.get('/cart',shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.get('/products/:productId', shopController.getProduct);
 router.get('/orders',shopController.getOrders);
 router.get('/checkout',shopController.getCheckout);
-
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 module.exports = router;
