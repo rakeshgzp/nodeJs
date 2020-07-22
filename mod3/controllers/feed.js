11a74c5eb7bc85d1a334187342728f17e496f22c
rakeshgzp <rprakeshprasad00@gmail.com>
2020-07-22 23:16:11 +0530

    Async await used as alternative to promises

--- mod3/controllers/feed.js
+++ mod3/controllers/feed.js
@@ -4,27 +4,26 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
     let totalItems;
-    Post.find().countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find().skip((currentPage - 1) * perPage)
+    try {
+    totalItems = await Post.find().countDocuments()
+    const posts = await Post.find().skip((currentPage - 1) * perPage)
                 .limit(perPage);
-        }) 
-        .then(posts => {
-            res.status(200).json({message:'Fetched post successfully!', posts: posts
-            , totalItems: totalItems})
-        })
 
-        .catch(err => {
-            if (!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err)                      // As it's inside async code, throwing of error won't work
-        })
+    res.status(200).json({
+        message:'Fetched post successfully!', posts: posts, 
+        totalItems: totalItems
+    })
+    }
+    catch(err){
+        if (!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err)                      // As it's inside async code, throwing of error won't work
+    }
 };
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
