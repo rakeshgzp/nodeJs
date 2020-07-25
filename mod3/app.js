ff49e8ea52bd163f4825e4ac78a6f2ec69d9b118
rakeshgzp <rprakeshprasad00@gmail.com>
2020-07-25 23:31:23 +0530

    Added graphql queries and mutations

--- mod3/app.js
+++ mod3/app.js
@@ -6,6 +6,11 @@ const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+var { graphqlHTTP } = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/is-auth');
+
 const fileStorage  = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
@@ -29,11 +34,35 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 })
 app.put('/path');
 app.use('/feed',feedRoutes);
 app.use('/auth',authRoutes);
+app.use(auth);
+app.use('/graphql', graphqlHTTP({
+    schema: graphqlSchema,
+    graphiql: true,
+    rootValue: graphqlResolver,
+    
+    formatError(err) {
+        if (!err.originalError){
+            console.log(err);
+            return err;
+        }
+        console.log(2);
+        const data = err.originalError.data;
+        const message = err.message || 'An error occurred.';
+        console.log(err.originalError);
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+    }
+})
+);
+
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
